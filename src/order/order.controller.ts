fb08d84cd74901e3db7f5695204581c165c3eed9
mattosoguilherme <guilhermemktfran@gmail.com>
2022-02-04 00:15:06 -0300

    commit

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,34 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Order } from '@prisma/client';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Role } from 'src/utils/roles.enum';
 import { OrderService } from './order.service';
-import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @Controller('order')
+@ApiTags('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
+  @Roles(Role.ADMIN, Role.USER)
+  @ApiOperation({ summary: 'Cria um pedido' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), RolesGuard)
+  create(@Body() createOrderDto: CreateOrderDto): Promise<Order> {
     return this.orderService.create(createOrderDto);
   }
 
   @Get()
+  @Roles(Role.ADMIN, Role.USER)
+  @ApiOperation({ summary: 'Listar todos os pedidos' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), RolesGuard)
   findAll() {
     return this.orderService.findAll();
   }
 
   @Get(':id')
+  @Roles(Role.ADMIN, Role.USER)
+  @ApiOperation({ summary: 'Listar pedido por ID' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), RolesGuard)
   findOne(@Param('id') id: string) {
-    return this.orderService.findOne(+id);
+    return this.orderService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(Role.ADMIN, Role.USER)
+  @ApiOperation({ summary: 'Atualizar pedido' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), RolesGuard)
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.orderService.update(+id, updateOrderDto);
+    return this.orderService.update(id, updateOrderDto);
   }
 
   @Delete(':id')
+  @Roles(Role.ADMIN, Role.USER)
+  @ApiOperation({ summary: 'Deletar um pedido por ID' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), RolesGuard)
   remove(@Param('id') id: string) {
-    return this.orderService.remove(+id);
+    return this.orderService.remove(id);
   }
 }
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -4,8 +4,10 @@ import { OrderController } from './order.controller';
 import { PrismaService } from 'src/prisma.service';
 import { RolesGuard } from './../auth/guards/roles.guard';
 import { Validator } from 'src/validation';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports:[PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [OrderController],
   providers: [OrderService, Validator,PrismaService,RolesGuard]
 })
