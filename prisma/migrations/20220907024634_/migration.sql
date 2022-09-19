0a5b3190043d88b0cc845242ddb728ce328b3e00
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-09-19 02:03:56 -0300

    .

--- prisma/migrations/20220907024634_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "order" ADD COLUMN     "observation" TEXT NOT NULL DEFAULT E'';
--- prisma/migrations/20220907171918_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `bill` on the `table` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `DoublePrecision`.
-
-*/
--- AlterTable
-ALTER TABLE "table" ALTER COLUMN "bill" SET DATA TYPE DOUBLE PRECISION;
